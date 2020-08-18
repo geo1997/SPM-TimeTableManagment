4cda1a0ab79dfadbcee3e4613b49f392144988d9
Haritha Dasanayake <Haritha Dasanayake@DESKTOP-DM1CECQ>
2020-08-18 19:02:37 +0530

    Minor changes of lecturer and subject

--- TimeTableManagment/Forms/SubjectForm.cs
+++ TimeTableManagment/Forms/SubjectForm.cs
@@ -100,6 +100,7 @@ namespace TimeTableManagment.Forms
                 numLabHr.Value = 0;
                 numTuteHr.Value = 0;
                 numEvoHr.Value = 0;
+                MessageBox.Show("Subject Information Added successfully", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
 
@@ -176,6 +177,7 @@ namespace TimeTableManagment.Forms
                 ExecuteQuery(updateQuery);
                 LoadData();
                 clearField();
+                MessageBox.Show("Subject Information Has Updated", "Update Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -188,9 +190,7 @@ namespace TimeTableManagment.Forms
             ExecuteQuery(deleteQuery);
             LoadData();
             clearField();
-
-
-
+            MessageBox.Show("Subject Information Deleted successfully", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         //Validating the form
