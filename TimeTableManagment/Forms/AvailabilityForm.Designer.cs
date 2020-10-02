f9f1f20d3b9c58cec2d7048b6b8ddbf6e2ef7f65
kgeor <kgeorgesilva97@gmail.com>
2020-10-02 14:04:18 +0530

    CHanges to availabilty form

--- TimeTableManagment/Forms/AvailabilityForm.Designer.cs
+++ TimeTableManagment/Forms/AvailabilityForm.Designer.cs
@@ -87,7 +87,7 @@
             this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.metroTabControl1.Location = new System.Drawing.Point(0, 0);
             this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.SelectedIndex = 1;
+            this.metroTabControl1.SelectedIndex = 0;
             this.metroTabControl1.Size = new System.Drawing.Size(1006, 653);
             this.metroTabControl1.TabIndex = 0;
             this.metroTabControl1.UseSelectable = true;
